087b9b33b778c7a829778f236bd62da1214c776c
github username <sharnellgrant@gmail.com>
2021-11-11 20:12:14 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+          <div key={index}>
+          {movie.title}{movie.time}
+          <p>Genre:</p>
+              <ul>
+                  {movie.genres.map((genre, index) => (
+                      <li key={index}>{genre}</li>
+                  ))}
+              </ul>
+          
+          </div>
+        ))}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
 
+
 render (
   <App />,
+  
   document.getElementById('root')
 );
